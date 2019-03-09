7623d3f03c4cd424c6e3fb1f83eab868ad6a57c4
4p0k <4chpok1991@mail.ru>
2019-03-09 18:29:42 +0300

    Refactoring strings arrays.

--- app/src/main/java/com/example/calculator/calculating.java
+++ app/src/main/java/com/example/calculator/calculating.java
@@ -12,10 +12,10 @@ public class calculating extends AppCompatActivity {
 
 
 
-    ArrayList<String> lstnum = new ArrayList<>();       //хеш выбраных знаков
-    ArrayList<Integer> lstsign = new ArrayList<>();     //хеш количество знаков
+    ArrayList<String> arrLisSelectTypeSign = new ArrayList<>();       //хеш выбраных знаков
+    ArrayList<Integer> arrLisNumberOfSign = new ArrayList<>();     //хеш количество знаков
     //boolean nohard = false;                             //хеш выбора сложности
-    ArrayList<String> arrnohard = new ArrayList<>();    //создание дом листа для промижуточных знаков
+    ArrayList<String> arrLisNoHard = new ArrayList<>();    //создание дом листа для промижуточных знаков
     String exampletxts ,extxt;                          //пример и правельный ответ
     StringBuilder extxtbut = new StringBuilder ();      //ответ пользователя
     boolean minus = false, end = false;                 //поставил ли пользователь минус и конец даного примера
@@ -26,19 +26,16 @@ public class calculating extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calculating);
 
-        lstnum = getIntent().getStringArrayListExtra("listnum");
-        lstsign = getIntent().getIntegerArrayListExtra("listsig");
+        arrLisSelectTypeSign = getIntent().getStringArrayListExtra("listnum");
+        arrLisNumberOfSign = getIntent().getIntegerArrayListExtra("listsig");
         //nohard = getIntent().getExtras().getBoolean("mult");
-        for (int i = 0; i < lstnum.size(); i++) {
 
-            if (2 >= Character.getNumericValue(lstnum.get( i ).charAt(0))) {
-
-                arrnohard.add(lstnum.get(i));
-            }else {break;}
+        for (int i = 0; 2 >= Character.getNumericValue(arrLisSelectTypeSign.get( i ).charAt(0)); i++) {
+            arrLisNoHard.add(arrLisSelectTypeSign.get(i));
         }
-        if (arrnohard.size() == 0) {
-            arrnohard.add("122");
-            arrnohard.add("222");
+        if (arrLisNoHard.size() == 0) {
+            arrLisNoHard.add("122");
+            arrLisNoHard.add("222");
         }
         random_example ();
     }
@@ -59,76 +56,75 @@ public class calculating extends AppCompatActivity {
         rz = (int)Math.floor(ten + Math.random() * (9 * ten));
         return rz;
     }
+    StringBuilder oneSignText = new StringBuilder ();
+    int oneSignInt;
 
-    public String[] get2Sign (int z,int nam1,int nam2) {        //создание одного примера
-        int ext;
-        StringBuilder textext = new StringBuilder ();
+    public void get2Sign (int z,int nam1,int nam2) {        //создание одного примера
+        oneSignInt = 0;
+        oneSignText.setLength(0);
 
         int n1 = getOneRand(nam1, z);
         int n2 = getOneRand(nam2, z);
         if (z == 1) {
-            ext = n1 + n2;
-            textext.append(n1).append(" + ").append(n2);
+            oneSignInt = n1 + n2;
+            oneSignText.append(n1).append(" + ").append(n2);
         } else if (z == 2) {
-            ext = n1 - n2;
-            textext.append(n1).append(" - ").append(n2);
+            oneSignInt = n1 - n2;
+            oneSignText.append(n1).append(" - ").append(n2);
         } else if (z == 3) {
-            ext = n1 * n2;
-            textext.append(n1).append(" * ").append(n2);
+            oneSignInt = n1 * n2;
+            oneSignText.append(n1).append(" * ").append(n2);
         }else {
             int n3 = n1 * n2;
-            ext = n1;
-            textext.append(n3).append(" / ").append(n2);
+            oneSignInt = n1;
+            oneSignText.append(n3).append(" / ").append(n2);
         }
-
-        String[] extmas = {Integer.toString(ext), textext.toString()};
-        return extmas;
     }
 
     public void random_example (){
         StringBuilder exampletxt = new StringBuilder ();                        //сбор примера
         int example = 0;                                                        //ответ
-        int numsign = lstsign.get( random.nextInt(lstsign.size()) );    //выбор количество знаков
+        int numsign = arrLisNumberOfSign.get( random.nextInt(arrLisNumberOfSign.size()) );    //выбор количество знаков
         String num;                                                             //код для создание одного примара 111 -> 2+3
-        String[] extmas;
+        //String[] extmas;
 
-        num = lstnum.get( random.nextInt(lstnum.size()) );
+        num = arrLisSelectTypeSign.get( random.nextInt(arrLisSelectTypeSign.size()) );
 
-        extmas = get2Sign(  Character.getNumericValue(num.charAt(0)),
-                            Character.getNumericValue(num.charAt(1)),
-                            Character.getNumericValue(num.charAt(2)) );
+        get2Sign(  Character.getNumericValue(num.charAt(0)),
+                    Character.getNumericValue(num.charAt(1)),
+                    Character.getNumericValue(num.charAt(2)) );
 
-        example = Integer.parseInt(extmas[0]);
+        example = oneSignInt;
         if (numsign == 1) {
-            exampletxt.append(extmas[1]);
+            exampletxt.append(oneSignText);
         }else {
-            exampletxt.append("( ").append(extmas[1]).append(" )");
+            exampletxt.append("( ").append(oneSignText).append(" )");
         }
 
         for (int i = 3; i <= numsign; i += 2) {
-            num = lstnum.get(random.nextInt(lstnum.size()));
+            num = arrLisSelectTypeSign.get(random.nextInt(arrLisSelectTypeSign.size()));
 
-            extmas = get2Sign(  Character.getNumericValue(num.charAt(0)),
-                                Character.getNumericValue(num.charAt(1)),
-                                Character.getNumericValue(num.charAt(2)) );
+            get2Sign(  Character.getNumericValue(num.charAt(0)),
+                        Character.getNumericValue(num.charAt(1)),
+                        Character.getNumericValue(num.charAt(2)) );
 
-            num = arrnohard.get(random.nextInt(arrnohard.size()));
+            num = arrLisNoHard.get(random.nextInt(arrLisNoHard.size()));
 
             if (Character.getNumericValue(num.charAt(0)) == 2) {
-                example -= Integer.parseInt(extmas[0]);
+                example -= oneSignInt;
                 exampletxt.append(" - ");
             } else if (Character.getNumericValue(num.charAt(0)) == 3) {
-                example *= Integer.parseInt(extmas[0]);
+                example *= oneSignInt;
                 exampletxt.append(" * ");
             } else {
-                example += Integer.parseInt(extmas[0]);
+                example += oneSignInt;
                 exampletxt.append(" + ");
             }
 
-            exampletxt.append("( ").append(extmas[1]).append(" )");
+            exampletxt.append("( ").append(oneSignText).append(" )");
         }
         if ( 0 == numsign % 2){
-            num = arrnohard.get(random.nextInt(arrnohard.size()));
+            num = arrLisNoHard.get(random.nextInt(arrLisNoHard.size()));
 
             if (Character.getNumericValue(num.charAt(0)) == 2) {
                 int randnum = getOneRand(Character.getNumericValue(num.charAt(2)),1);
